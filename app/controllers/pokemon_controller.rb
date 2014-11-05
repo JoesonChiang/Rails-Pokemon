f2d464c2ef5719d9791329f32d44d5f8982c8895
Joeson Chiang <joesonchiang@berkeley.edu>
2014-11-04 22:19:31 -0800

    Pokemon MASTERRRR

---
+++ app/controllers/pokemon_controller.rb
@@ -0,0 +1,36 @@
+class PokemonController < ApplicationController
+
+	#couldn't get the create to work. For some reason, I keep getting an error saying that 
+	# rails could not find a "new" method even though I specified the route. I figured this controller would
+	# be able to inherit the new method frmo the application controller, but I might just have a routing error or something.
+	def create
+		@pokemon = Pokemon.new({:name => (params[:pokemon])[:name], 
+        	:trainer_id => current_trainer, :level => 1, :health => 100})
+		# @pokemon.name = :name
+		@pokemon.trainer = current_trainer
+	    if @pokemon.save
+			redirect_to trainer_path(current_trainer)
+		else
+			redirect_to pokemon_new_path
+			flash[:error] = @pokemon.errors.full_messages.to_sentence
+		end
+	end
+
+	def capture
+		@pokemon = Pokemon.find(params[:id])
+		@pokemon.trainer = current_trainer
+		@pokemon.save
+    	redirect_to root_path
+	end
+
+	def damage
+		@pokemon = Pokemon.find(params[:id])
+		@pokemon.health -= 10
+		@pokemon.save
+		if @pokemon.health <= 0
+			@pokemon.destroy
+		end
+    	redirect_to trainer_path(current_trainer)
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :trainers
   resources :trainers
+
+  # capture GET '/pokemon/:trainer_id', to: 'pokemon#capture'
+  patch '/capture', to: 'pokemon#capture', as: "capture"
+
+  patch '/damage', to: 'pokemon#damage', as: "damage"
+
+  post '/pokemon', to: 'pokemon#create'
+
+  get '/pokemon/new', to: 'pokemon#new', as: 'pokemon_new'
 end
